417f1c24c71e536f72ecfae9301e2e5809dd864b
Simon Bateson <simon.bateson@blackcats.dev>
2021-07-21 10:15:07 +0100

    Add processing information columns to Staging and Reporting tables

--- Strava DB Objects/Tables/reporting.DIM_activity_type.sql
+++ Strava DB Objects/Tables/reporting.DIM_activity_type.sql
@@ -1,4 +1,5 @@
 ﻿CREATE TABLE Reporting.DIM_activity_type (
 activity_type_id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-activity_type NVARCHAR(50) 
+activity_type NVARCHAR(50),
+date_created DATETIME DEFAULT GETDATE()
 );
